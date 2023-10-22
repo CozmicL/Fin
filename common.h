de8643ebdd3b0fee222c7bd66b24193d24c080db
Tobias Faucher <supertjfaucher@gmail.com>
2023-10-22 15:19:22 -0600

    first commit

---
+++ common.h
@@ -0,0 +1,12 @@
+#ifndef fin_common_h
+#define fin_common_h
+
+#include "stdbool.h"
+#include "stddef.h"
+#include "stdint.h"
+
+#define DEBUG_PRINT_CODE
+#define DEBUG_TRACE_EXECUTION
+
+
+#endif
---
+++ compiler.h
@@ -0,0 +1,8 @@
+#ifndef fin_compiler_h
+#define fin_compiler_h
+#include "object.h"
+#include "vm.h"
+
+bool compile(const char* source,Chunk* chunk);
+
+#endif
