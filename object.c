d2059a12ab04348fd4aee6b3119c5a5f7e0bd3da
Tobias Faucher <supertjfaucher@gmail.com>
2023-10-24 23:15:07 -0600

    new file:   examples/calc.fin 	renamed:    calc.fin -> examples/strings.fin 	modified:   main.exe 	modified:   object.c 	modified:   object.h 	new file:   table.c 	new file:   table.h

--- object.c
+++ object.c
@@ -19,22 +19,34 @@ static Obj* allocateObject(size_t size, ObjType type){
     return object;
 }
 
-static ObjString* allocateString(char* chars, int len) {
+static ObjString* allocateString(char* chars, int len, uint32_t hash) {
     ObjString* string = ALLOCATE_OBJ(ObjString,OBJ_STRING);
     string->length = len;
     string->chars = chars;
+    string->hash = hash;
     return  string;
 }
 
+static uint32_t hashString(const char* key, int length){
+    uint32_t hash = 216613626lu;
+    for(int i=0; i<length; i++){
+        hash ^= (uint32_t)key[i];
+        hash *= 16777619;
+    }
+    return hash;
+}
+
 ObjString* takeString(char* chars, int length){
-    return allocateString(chars, length);
+    uint32_t hash = hashString(chars, length);
+    return allocateString(chars, length,hash);
 }
 
 ObjString* copyString(const char* chars, int len) {
+    uint32_t hash = hashString(chars, len);
     char* heapChars = ALLOCATE(char, len + 1);
     memcpy(heapChars, chars, len);
     heapChars[len] = '\0';
-    return allocateString(heapChars, len);
+    return allocateString(heapChars, len,hash);
 }
 
 void printObject(Value value){
